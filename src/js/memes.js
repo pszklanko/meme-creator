d6e41c9e65f312ccf0032a679caf3e78454ada62
Piotr Szklanko <pszklanko@gmail.com>
2018-03-11 17:53:42 +0100

    loaders for js and css

--- src/js/memes.js
+++ src/js/memes.js
@@ -1,2 +1,9 @@
 import './general';
 console.log('memes.js')
+class Memes {
+    constructor() {
+        console.log('memes class');
+    }
+}
+
+new Memes();
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,23 @@ module.exports = {
     output: {
         path: __dirname + "/dist",
         filename: '[name].js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'es2015'],
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ],
+    },
 }
